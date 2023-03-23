f20186d44fe04e1e743049c9cbff1738645dd7da
Karim Mk <mk47blast@protonmail.com>
2023-03-23 01:13:43 +0100

    Priting to Errors to Standrad error.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Applicatoin error: {}", e);
+        eprintln!("Applicatoin error: {}", e);
         process::exit(1);
     }
 }
