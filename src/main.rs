c3e20e8bc178da061151335794c9abad4041af8d
Karim Mk <mk47blast@protonmail.com>
2023-03-23 01:04:02 +0100

    fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use grep::Config;
+use minigrep::Config;
 use std::env;
 use std::process;
 
@@ -10,7 +10,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = grep::run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Applicatoin error: {}", e);
         process::exit(1);
     }
